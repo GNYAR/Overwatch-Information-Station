63390110f03899a2e123dbf73d7bc0b1e86e13e1
GNYAR <s901121s901121@gmail.com>
2022-10-12 14:51:44 +0800

    Initial Commit

---
+++ OverwatchInformationStation/OverwatchInformationStationApp.swift
@@ -0,0 +1,17 @@
+//
+//  OverwatchInformationStationApp.swift
+//  OverwatchInformationStation
+//
+//  Created by User20 on 2022/10/12.
+//
+
+import SwiftUI
+
+@main
+struct OverwatchInformationStationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
