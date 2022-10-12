4b8a4f995fb8e668d61ba02a5abaf9f6e9606d57
GNYAR <s901121s901121@gmail.com>
2022-10-12 18:02:04 +0800

    feat(home): implement the tab view #3

---
+++ OverwatchInformationStation/pages/Heros.swift
@@ -0,0 +1,20 @@
+//
+//  Heros.swift
+//  OverwatchInformationStation
+//
+//  Created by User20 on 2022/10/12.
+//
+
+import SwiftUI
+
+struct Heros: View {
+  var body: some View {
+    Text("Heros")
+  }
+}
+
+struct Heros_Previews: PreviewProvider {
+  static var previews: some View {
+    Heros()
+  }
+}
---
+++ OverwatchInformationStation/pages/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  OverwatchInformationStation
+//
+//  Created by User20 on 2022/10/12.
+//
+
+import SwiftUI
+
+struct Home: View {
+  var body: some View {
+    Text("Home")
+  }
+}
+
+struct Home_Previews: PreviewProvider {
+  static var previews: some View {
+    Home()
+  }
+}
